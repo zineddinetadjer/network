04625b4dda18ec4ff86b19aaec40028070b440c1
zineddinetadjer <zinedinetadjer1@gmail.com>
2022-11-11 15:51:11 +0100

    Update index.js

--- socket/index.js
+++ socket/index.js
@@ -24,7 +24,6 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     //remove user from active users
     activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
-    console.log("user disconnected", activeUsers);
     //send all active users to all users
     io.emit("get-users", activeUsers);
   });
@@ -33,8 +32,6 @@ io.on("connection", (socket) => {
   socket.on("send-message", (data) => {
     const { receiverId } = data;
     const user = activeUsers.find((user) => user.userId === receiverId);
-    console.log("sending from socket", receiverId);
-    console.log("data", data);
     if (user) {
       io.to(user.socketId).emit("receive-message", data);
     }
